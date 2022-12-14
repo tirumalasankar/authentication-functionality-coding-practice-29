fbc118316b47fdd68e57c1d773d0dfdf9ee4b58a
tirumalasankar <tirumalavurranki@gmail.com>
2022-12-14 09:12:26 +0000

    authentication functionality

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+
+import Header from '../Header'
+
+import './index.css'
+
+const About = () => (
+  <>
+    <Header />
+    <div className="about">
+      <h1>About Route</h1>
+    </div>
+  </>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => (
+  <>
+    <Header />
+    <div className="home">
+      <h1>Home Route</h1>
+    </div>
+  </>
+)
+export default Home
